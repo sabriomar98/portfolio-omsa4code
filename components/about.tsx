b1204093ada85d7d30623c08a5b43c037a12a22f
sabriomar <os@adliket.com>
2024-07-14 00:12:27 +0100

    change the about page to my infos

--- components/about.tsx
+++ components/about.tsx
@@ -17,12 +17,19 @@ export default function About() {
       transition={{ delay: 0.175 }}
       id="about"
     >
-      <SectionHeading>About me</SectionHeading>
+      <SectionHeading>About Me</SectionHeading>
       <p className="mb-3">
         After graduating with a degree in{" "}
-        <span className="font-medium">Accounting</span>, I decided to pursue my
-        passion for programming. I enrolled in a coding bootcamp and learned{" "}
-        <span className="font-medium">full-stack web development</span>.{" "}
+        <span className="font-medium">Software Engineering</span> from{" "}
+        <span className="font-medium">Faculty of Science and Technology, (Errachidia | Morocco)</span>,
+        and completing a Master in{" "}
+        <span className="font-medium">Big Data and Decision Support</span> at{" "}
+        <span className="font-medium">National School of Applied Sciences, (Khouribga | Morocco)</span>,
+        I have been passionate about programming and development. I have professional experience
+        as a{" "}
+        <span className="font-medium">Full-Stack Developer</span> at AdLikeT Morocco and as a{" "}
+        <span className="font-medium">JEE Consultant</span> at Fortil Group,
+        where I worked on various backend and frontend projects.
         <span className="italic">My favorite part of programming</span> is the
         problem-solving aspect. I <span className="underline">love</span> the
         feeling of finally figuring out a solution to a problem. My core stack
@@ -30,20 +37,11 @@ export default function About() {
         <span className="font-medium">
           React, Next.js, Node.js, and MongoDB
         </span>
-        . I am also familiar with TypeScript and Prisma. I am always looking to
-        learn new technologies. I am currently looking for a{" "}
-        <span className="font-medium">full-time position</span> as a software
-        developer.
+        . I am also familiar with TypeScript, Prisma, and a wide range of other
+        technologies including Spring Boot, Angular, and various database systems.
+        I am always looking to learn new technologies.
       </p>
 
-      <p>
-        <span className="italic">When I'm not coding</span>, I enjoy playing
-        video games, watching movies, and playing with my dog. I also enjoy{" "}
-        <span className="font-medium">learning new things</span>. I am currently
-        learning about{" "}
-        <span className="font-medium">history and philosophy</span>. I'm also
-        learning how to play the guitar.
-      </p>
     </motion.section>
   );
 }
